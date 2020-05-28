45bea656e1d67c666d4d596c3b160e714c033ce8
Anugerah <65181197+nugehood@users.noreply.github.com>
2020-05-29 01:35:03 +0800

    Rehesal first version

---
+++ WatcherGame/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    public Light light;
+    public float intensity;
+    public float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = intensity;
+        StartCoroutine(flicker(seconds));
+    }
+
+    public IEnumerator flicker(float second)
+    {
+        intensity = 1.003022e+07f;
+        yield return new WaitForSeconds(second);
+        intensity = 1.003022e+06f;
+        
+    }
+}
